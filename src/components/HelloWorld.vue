21f8c9ebdaeece14b821e067188a9aedd6e94b12
andypike7 <tech@sym.ru>
2020-09-22 11:29:32 +0300

    Starting: vuetify and basic axios requests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-bottom-sheet v-model="isSheetVisible">
+        <v-sheet class="text-center">
+          <div style="height:20px" />
+          <div>{{ sheetText }}</div>
+          <div class="my-2 mb-0">
+            <v-btn
+              small
+              color="error"
+              class="my-2 mb-6"
+              @click="isSheetVisible = !isSheetVisible"
+            >
+              Retry
+            </v-btn>
+          </div>
+        </v-sheet>
+      </v-bottom-sheet>
+      <v-col class="mb-4">
+        <!-- <h1-- class="display-2 font-weight-bold mb-3">
+          Welcome to Tada-Chat
+        </h1-->
+        Settings:
+        {{ settings }}
+        <hr>
+        Rooms:
+        {{ rooms }}
+        <hr>
+        Messages:
+        {{ messages }}
+        <hr>
+
+        <!-- Комнаты:
+        <v-select
+          dark
+          filled
+          :items="rooms"
+          value="Second"
+          @change="changeRoom"
+        /> -->
+        <v-textarea
+          label="Введите сообщение"
+          auto-grow
+          autofocus
+          clearable
+          background-color="#ddf"
+        />
+
+        <v-btn
+          :disabled="sendDisabled"
+          :loading="sendDisabled"
+          @click="sendMessage"
+        >
+          Send a Message
+        </v-btn>
+
+      </v-col>
+
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  import axios from 'axios';
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  const apiUrl = 'https://nane.tada.team/api';
+
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+      sendDisabled: false,
+      settings: {},
+      messages: [],
+      rooms: [],
+      currentRoom: null,
+      isSheetVisible: false,
+      sheetText: null,
+      selectedRoom: null,
+    }),
+
+    beforeMounted() {
+      console.clear();
+    },
+    async mounted() {
+      await this.getSettings();
+      await this.getRooms();
+
+      if (this.rooms.length === 0) {
+        alert('we have no rooms!');
+      }
+      else {
+        console.log('*** GET:', this.rooms[0].name);
+        this.refreshARoom(this.rooms[0].name);
+        alert(`we have ${this.rooms.length} room(s)!`);
+      }
+      this.getMessages();
+    },
+    methods: {
+      async getSettings() {
+        const that = this;
+
+        this.isSheetVisible = false;
+
+        await axios.get(`${apiUrl}/settings`)
+          .then(response => {
+            that.settings = response.data.result;
+          })
+          .catch(() => {
+            this.sheetText = 'Cannot get the chat settings.';
+            this.isSheetVisible = true;
+          });
+      },
+      async getRooms() {
+        const that = this;
+
+        await axios.get(`${apiUrl}/rooms`)
+          .then(response => {
+            console.log('*** Rooms:', response.data.result)
+            that.rooms = response.data.result;
+          })
+          .catch(() => {
+            this.sheetText = 'Cannot get the list of rooms.';
+            this.isSheetVisible = true;
+          });
+      },
+      async refreshARoom(roomName) {
+        alert('refreshARoom: ' + roomName);
+        this.currentRoom = roomName;
+        console.log(`currentRoom = ${roomName}`);
+      },
+      sendMessage() {
+        this.sendDisabled = true;
+        setTimeout(() => {
+          this.sendDisabled = false;
+        }, 1000);
+      },
+      changeRoom() {
+        alert('changeRoom');
+      },
+      getMessages() {
+        /*
+        axios
+          .get(`${apiUrl}/rooms/${this.currentRoom}/history`)
+          .then(response => console.log('*** RESP:', response))
+          .error(err => console.log('*** ERROR:', err));
+        /*
+        this.messages = {
+          "result":[
+            {
+              "name":"kozma",
+              "last_message":{
+                "room":"kozma",
+                "created":"2020-09-20T08:24:31.724098867Z",
+                "sender":{
+                  "username":"Козьма Прутков"
+                },
+                "text":"Сребролюбцы! сколь ничтожны ваши стяжания, коли все ваши сокровища не стоят одного листка из лаврового венка поэта!"
+              },
+            },
+          ],
+        };
+        */
+      },
+    },
+  }
+</script>
